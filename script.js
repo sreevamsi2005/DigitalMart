7dd18b9c2a389da180c95fa9addbad2faffd3ddc
Navyasri2005 <161459407+Navyasri2005@users.noreply.github.com>
2024-02-28 15:27:51 +0530

    Add files via upload
    This a responsive login and signup page for our website

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const close = document.querySelector(".close");
-const open = document.querySelector(".ham");
-const menu = document.querySelector(".menu");
-close.addEventListener("click", () => {
-  menu.style.visibility = "hidden";
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
 });
-open.addEventListener("click", () => {
-  menu.style.visibility = "visible";
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
+});
+// Toggle password visibility
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function () {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    togglePassword.classList.toggle('fa-eye-slash'); // Toggle the eye slash icon
 });
