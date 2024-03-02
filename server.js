f997807f54b27c4858e9fc17b29c2818a41f3e1e
GayathriR31200403 <161404261+GayathriR31200403@users.noreply.github.com>
2024-03-02 13:26:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/',(req,res)=>{
     res.render('main')
 })
 app.post('/payment', (req, res) => {
-    // Handle payment logic here (if needed)
+   
     res.render('payment');
 });
 app.post('/complete-payment', (req, res) => {
@@ -18,17 +18,17 @@ app.post('/complete-payment', (req, res) => {
 
         if (!selectedPayment) {
             throw new Error('Please select a payment method');
-        }
-
-        // Your logic for processing the payment
-
-        // Assuming the payment is successful, render the success page
+        } 
         res.render('paymentsuccess');
     } catch (error) {
         console.error(error);
         res.status(400).render('error', { errorMessage: error.message });
     }
 });
+app.post('/cart',(req,res)=>{
+    res.render('cart.ejs')
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
